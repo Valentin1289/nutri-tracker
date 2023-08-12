67065ec723bc74388271b5bfb2a6864cd97f69e9
Valentin1289 <valibaiacof1289@gmail.com>
2023-08-12 14:10:05 +0300

    .

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from "./App";
 import "./index.css";
 
+
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
